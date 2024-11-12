7c3e7e2db0061bc45d6347c9eef25f1449caf65d
AILE\mahmutcelik <>
2024-11-12 14:36:22 +0300

    first commit

---
+++ src/main/java/com/cekino/rabbitsimulation/RabbitsimulationApplication.java
@@ -0,0 +1,13 @@
+package com.cekino.rabbitsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitsimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitsimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cekino/rabbitsimulation/RabbitsimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cekino.rabbitsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitsimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
